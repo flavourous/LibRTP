70e194ae37d32bda70142f0a66a0cadf6fe40445
David <d_higgins@imap.cc>
2015-12-17 10:09:26 +0000

    Library for getting repeating time patterns.  First commit...happy with it so far...got tests.

---
+++ LibRTP.Test/Program.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace LibRTP.Test
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Ad hock "testing framework"
+			var opt = new OnOfPatternTests ();
+			opt.RunTests ();
+			var ept = new EveryPatternTests ();
+			ept.RunTests ();
+			Console.WriteLine ("Yayyy tests passed!!");
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.WriteLine (new String ('G', Console.WindowWidth-1));
+			Console.WriteLine (new String ('r', Console.WindowWidth-1));
+			Console.WriteLine (new String ('e', Console.WindowWidth-1));
+			Console.WriteLine (new String ('e', Console.WindowWidth-1));
+			Console.WriteLine (new String ('n', Console.WindowWidth-1));
+			Console.ReadKey ();
+		}
+	}
+	abstract class TestCase
+	{
+		protected abstract IRecurr d { get; }
+		readonly DateTime s;
+		readonly DateTime e;
+		readonly List<DateTime> res;
+
+		private TestCase(DateTime testStart, DateTime testEnd) { s = testStart; e = testEnd; }
+
+		public TestCase(IEnumerable<DateTime> expectedResults,DateTime testStart, DateTime testEnd) : this(testStart, testEnd)
+		{ res=new List<DateTime>(expectedResults); }
+		public void AssertCase()
+		{
+			List<DateTime> test = new List<DateTime> (d.GetOccurances (s, e));
+			Debug.Assert (test.Count == res.Count);
+			for (int i = 0; i < test.Count; i++)
+				Debug.Assert (test [i] == res [i], test [i].ToShortDateString () + " is not the expected " + res [i].ToShortDateString ());
+		}
+
+		readonly String expectedFailure;
+		public TestCase(String expectedFailure,DateTime testStart, DateTime testEnd) : this(testStart, testEnd)
+		{ this.expectedFailure=expectedFailure; }
+		public void AssertFail()
+		{
+			Exception ec = null;
+			try {  var getit = d; }//create it
+			catch(Exception e) { ec = e; }
+			Debug.Assert(ec is ArgumentException && (ec as ArgumentException).ParamName == expectedFailure);
+		}
+	}
+
+	class EveryPatternTests
+	{
+		class EveryTestCase : TestCase
+		{
+			Func<RecurrsEveryPattern> _d;
+			protected override IRecurr d { get { return _d(); } }
+			public EveryTestCase(DateTime fx, int f, RecurrSpan t, DateTime? l, DateTime? u , DateTime testStart, DateTime testEnd, IEnumerable<DateTime> expectedResults)
+				:base(expectedResults, testStart,testEnd)
+			{ _d =()=> new RecurrsEveryPattern(fx,f,t,l,u); }
+			public EveryTestCase(DateTime fx, int f, RecurrSpan t, DateTime? l, DateTime? u , DateTime testStart, DateTime testEnd, String exprectedError)
+				:base(exprectedError, testStart,testEnd)
+			{ _d =()=> new RecurrsEveryPattern(fx,f,t,l,u); }
+		}
+		List<TestCase> cases = new List<TestCase> {
+			// Some on-every test cases...
+			new EveryTestCase(new DateTime(2015,2,12),1,RecurrSpan.Day,null,null,new DateTime(1950,1,1),new DateTime(1950,1,5),
+				new[] { new DateTime(1950,1,1),new DateTime(1950,1,2),new DateTime(1950,1,3),new DateTime(1950,1,4),new DateTime(1950,1,5) }
+			),
+			new EveryTestCase(new DateTime(2015,2,3),1,RecurrSpan.Week,null,null,new DateTime(2015,2,10),new DateTime(2015,2,24),
+				new[] {new DateTime(2015,2,10),new DateTime(2015,2,17),new DateTime(2015,2,24) }
+			),
+			new EveryTestCase(new DateTime(2014,2,3),1,RecurrSpan.Month,null,null,new DateTime(2018,1,1),new DateTime(2018,4,1),
+				new[] {new DateTime(2018,1,3),new DateTime(2018,2,3),new DateTime(2018,3,3),}
+			),
+			// leap year handling madness
+			new EveryTestCase(new DateTime(2012,2,29),1,RecurrSpan.Year,null,null,new DateTime(2018,1,1),new DateTime(2022,1,1),
+				new[] {new DateTime(2018,3,1),new DateTime(2019,3,1),new DateTime(2020,2,29),new DateTime(2021,3,1)}
+			),
+			// impossible month handling
+			new EveryTestCase(new DateTime(2010,1,31),1,RecurrSpan.Month,null,null,new DateTime(2012,1,1),new DateTime(2012,5,1),
+				new[] {new DateTime(2012,1,31),new DateTime(2012,2,29),new DateTime(2012,3,31),new DateTime(2012,4,30)}
+			),
+			// detect long range code changes at aleast...didnt confirm if correct! lol.
+			new EveryTestCase(new DateTime(2015,2,12),1,RecurrSpan.Week,null,null,new DateTime(1950,1,1),new DateTime(1950,1,20),
+				new[] { new DateTime(1950,1,4),new DateTime(1950,1,11),new DateTime(1950,1,18) }
+			),
+			//thatll do for nows.
+		};
+		List<TestCase> failedContracts = new List<TestCase> {
+		};
+		public void RunTests()
+		{
+			foreach (var c in cases) 
+				c.AssertCase ();
+			foreach (var c in failedContracts)
+				c.AssertFail ();
+		}
+	}
+	class OnOfPatternTests
+	{
+		class OnTestCase : TestCase
+		{
+			Func<RecurrsOnPattern> _d;
+			protected override IRecurr d { get { return _d(); } }
+			public OnTestCase(int[] onIndexes, RecurrSpan unitsMask, DateTime patStart, DateTime patEnd, DateTime testStart, DateTime testEnd, IEnumerable<DateTime> expectedResults)
+				:base(expectedResults, testStart,testEnd)
+			{ _d =()=> new RecurrsOnPattern(onIndexes, unitsMask, patStart, patEnd); }
+			public OnTestCase(int[] onIndexes, RecurrSpan unitsMask, DateTime patStart, DateTime patEnd, DateTime testStart, DateTime testEnd, String exprectedError)
+				:base(exprectedError, testStart,testEnd)
+			{ _d =()=> new RecurrsOnPattern(onIndexes, unitsMask, patStart, patEnd); }
+		}
+		List<TestCase> cases = new List<TestCase> {
+			// for single concecutive pairs - pushing the last possible onindex
+			new OnTestCase (
+				new[] { 28,1 }, RecurrSpan.Day | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				new [] { new DateTime (2015, 1, 28), new DateTime (2015, 2, 28), new DateTime (2015, 3, 28) }
+			),
+			new OnTestCase (
+				new[] { 28,4 }, RecurrSpan.Day | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 10, 1),
+				new [] { new DateTime (2015, 1, 28), new DateTime (2015, 5, 28), new DateTime (2015, 9, 28) }
+			),
+			new OnTestCase (
+				new[] { 365,1 }, RecurrSpan.Day | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2017, 12, 31),
+				new [] { new DateTime (2015, 12, 31), new DateTime (2016, 12, 30), new DateTime (2017, 12, 31) }
+			),
+			new OnTestCase (
+				new[] { 365,2 }, RecurrSpan.Day | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2017, 12, 31),
+				new [] { new DateTime (2015, 12, 31), new DateTime (2017, 12, 31) }
+			),
+			new OnTestCase (
+				new[] { 7,1 }, RecurrSpan.Day | RecurrSpan.Week, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 1, 31),
+				new [] { new DateTime (2015, 1, 4), new DateTime (2015, 1, 11), new DateTime (2015, 1, 18), new DateTime (2015, 1, 25) }
+			),
+			new OnTestCase (
+				new[] { 4,1 }, RecurrSpan.Week | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				new [] { new DateTime (2015, 1, 28), new DateTime (2015, 2, 28), new DateTime (2015, 3, 28) }
+			),
+			new OnTestCase (
+				new[] { 52,1 }, RecurrSpan.Week | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2017, 1, 1),
+				new [] { new DateTime (2015, 12, 28), new DateTime (2016, 12, 26) }
+			),
+			new OnTestCase(
+				new[] { 12,1 }, RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 12, 1), new DateTime (2017, 12, 1),
+				new [] { new DateTime (2015, 12, 1), new DateTime (2016, 12, 1), new DateTime (2017, 12, 1) }
+			),
+			// Triplets
+			new OnTestCase(
+				new[] { 4,3,1 }, RecurrSpan.Week | RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2017, 4, 1),
+				new [] { new DateTime (2015, 3, 28), new DateTime (2016, 3, 28), new DateTime (2017, 3, 28) }
+			),
+			new OnTestCase(
+				new[] { 4,3,2 }, RecurrSpan.Week | RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2017, 4, 1),
+				new [] { new DateTime (2015, 3, 28), new DateTime (2017, 3, 28) }
+			),
+			new OnTestCase(
+				new[] { 4,4,1 }, RecurrSpan.Day | RecurrSpan.Week | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				new [] { new DateTime (2015, 1, 29), new DateTime (2015, 2, 26), new DateTime (2015, 3, 26) }
+			),
+			new OnTestCase(
+				new[] { 4,4,6 }, RecurrSpan.Day | RecurrSpan.Week | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 12, 1),
+				new [] { new DateTime (2015, 1, 29), new DateTime (2015, 7, 30) }
+			),
+			// Quaddie
+			new OnTestCase(
+				new[] { 7,4,12,1 }, RecurrSpan.Day | RecurrSpan.Week | RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2018, 4, 1),
+				new [] { new DateTime (2016, 1, 3), new DateTime (2017, 1, 1), new DateTime (2017, 12, 31) }
+			),
+			new OnTestCase(
+				new[] { 7,4,12,3 }, RecurrSpan.Day | RecurrSpan.Week | RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2019, 4, 1),
+				new [] { new DateTime (2016, 1, 3), new DateTime (2018, 12, 30)}
+			),
+
+		
+		};
+
+		List<TestCase> failedContracts = new List<TestCase>
+		{
+			// out of ranges
+			new OnTestCase (
+				new[] { 13,1 }, RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes[0]"),
+			new OnTestCase (
+				new[] { 53,1 }, RecurrSpan.Week | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes[0]"),
+			new OnTestCase (
+				new[] { 5,1 }, RecurrSpan.Week | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes[0]"),
+			new OnTestCase (
+				new[] { 366,1 }, RecurrSpan.Day | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes[0]"),
+			new OnTestCase (
+				new[] { 29,1 }, RecurrSpan.Day | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes[0]"),
+			new OnTestCase (
+				new[] { 8,1 }, RecurrSpan.Day | RecurrSpan.Week, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes[0]"),
+			new OnTestCase (
+				new[] { 0,1 }, RecurrSpan.Day | RecurrSpan.Month, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes[0]"),
+
+			// invalid comvinations
+			new OnTestCase ( // too few indexes
+				new[] { 29 }, RecurrSpan.Day | RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"unitsMask"),
+			new OnTestCase (//to many indexes
+				new[] { 29, 29, 29, 29 }, RecurrSpan.Day | RecurrSpan.Month | RecurrSpan.Year, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"unitsMask"),
+			new OnTestCase ( // mask too short/too many indexes
+				new[] { 1,1 }, RecurrSpan.Day, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"unitsMask"),
+			new OnTestCase ( // no empty indexes
+				new int[] { }, RecurrSpan.Day, 
+				DateTime.MinValue, DateTime.MaxValue, new DateTime (2015, 1, 1), new DateTime (2015, 4, 1),
+				"onIndexes.Length"),
+		};
+		public void RunTests()
+		{
+			foreach (var c in cases) 
+				c.AssertCase ();
+			foreach (var c in failedContracts)
+				c.AssertFail ();
+		}
+	}
+}
---
+++ LibRTP/LibRTP.cs
@@ -0,0 +1,270 @@
+﻿using System;
+
+// We will deal with thse situations - I think they may be generalisable.
+
+// 1) Recurrance pattern.  Given a start (and possibly ending/truncation) date, specify one of these patterns:
+//				-- Repeat every N Days/Weeks/Months/Years
+//				-- Repeat on the Ith Day/Week/Month of each Week/Month/Year
+// the lib should be able to be queried for occurances with a daterange
+
+// 2) Region Patterns. Given a initial date (and a "rounding" param i guess), and an array of [ SpanUnit, SpanNumber ],
+//		the lib should be queriable for the array index from a input date corresponding to the position on that mask.  Rounding would
+//		simply help if specced by moving the initial date to a round number of the units of the first array type.
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace LibRTP
+{
+	// I omitted none=0 from the enum to guide users.
+	[Flags]
+	public enum RecurrSpan : uint { Day = 1, Week = 2, Month = 4, Year = 8 }; // none is special
+	delegate DateTime DateTimeShifter(DateTime src, int val);
+	class RUI {
+		public readonly int MaxValue;
+		public readonly DateTimeShifter CreateAtValue, NextValue;
+		public RUI(int mv, DateTimeShifter createAtValue,  DateTimeShifter nextValue) {
+			MaxValue = mv;
+			CreateAtValue = createAtValue;
+			NextValue = nextValue;
+		}
+	}
+	delegate DateTime DateTimeMover(DateTime Fixed, DateTime At, int frequency);
+	class RMOD {
+		public readonly DateTimeMover ClosestToAt;
+		public readonly DateTimeMover NextValue;
+		public RMOD(DateTimeMover ClosestToAt, DateTimeMover NextValue)
+		{
+			this.ClosestToAt = ClosestToAt;
+			this.NextValue = NextValue;
+		}
+	}
+	// week starts on bloddy monday, srsly.  There's probabbly a ll8n issue here.
+	static class Helpers
+	{
+		// this one is the the "every" types.  We have a method to modulo the closest repetition from a fixed date to a chosen date
+		public static Dictionary<RecurrSpan,RMOD> Moduliser = new Dictionary<RecurrSpan, RMOD> {
+			{ RecurrSpan.Day, new RMOD(
+				(fixedDate, atDate, f) => atDate.AddDays((int)(atDate - fixedDate).TotalDays % f),
+				(d, f, v) => d.AddDays(v)
+			)},
+			{ RecurrSpan.Week, new RMOD(
+				(fixedDate, atDate, f) => atDate.AddDays((int)(atDate - fixedDate).TotalDays % 7*f),
+				(d,f, v) => d.AddDays(v*7)
+			)},
+			{ RecurrSpan.Month, new RMOD(
+				(fixedDate, atDate, f) => 
+				{
+					// difference in years.
+					int ydiff = atDate.Year - fixedDate.Year;
+					int monthdiff;
+					if(ydiff == 0) monthdiff = fixedDate.Month - atDate.Month; // simple case.
+					else 
+					{
+						// months in complete years apart
+						monthdiff = 12*(ydiff-1);
+						// add the months in the 2 years that fall on the dates
+						if(ydiff > 0) monthdiff += 12 - fixedDate.Month + atDate.Month;
+						if(ydiff < 0) monthdiff -= 12 - atDate.Month + fixedDate.Month; // its also a negative value.
+					}
+					DateTime closestMonth = new DateTime(atDate.Year, atDate.Month, fixedDate.Day);
+					atDate.AddMonths(monthdiff % f);
+					return closestMonth;
+				},
+				(d,f, v) => {
+					var ret = d.AddMonths(v);
+					if(ret.Day != f.Day)
+					{
+						var aret = ret.AddDays(f.Day-ret.Day);
+						if(aret.Month == ret.Month)
+							ret=aret;
+					}
+					return ret;
+				}
+			)},
+			{ RecurrSpan.Year, new RMOD(
+				(fixedDate, atDate, f) => {
+					int closestYear =atDate.Year+ (atDate.Year - fixedDate.Year) % f;
+					return new DateTime(closestYear, 1,1).AddDays(fixedDate.DayOfYear - 1);
+				}, 
+				(d,f,v) => {
+					var ret = d.AddYears(v);
+					if(f.DayOfYear != ret.DayOfYear)
+					{
+						var aret = ret.AddDays(f.DayOfYear - ret.DayOfYear);
+							if(aret.Year == ret.Year)
+								ret=aret;
+					}
+					return ret;
+				}
+			)}
+		};
+		// These figures define the allowed repeating "on" types - they are a lower limit that are applicabale to all months, leap years, etc.
+		// In theory, though I wont do it, you can, since the pairs fall on unique indexes, use an array of length 15 (it's (biggest bit >> 1) - 1)
+		public static Dictionary<RecurrSpan,RUI> Units = new Dictionary<RecurrSpan, RUI> {
+			{ RecurrSpan.Year  | RecurrSpan.Month, new RUI (12, 
+				(d, v) => d.AddMonths (v - d.Month),
+				(d,n) => d.AddYears(n)
+			)},
+			{ RecurrSpan.Year  | RecurrSpan.Week, new RUI (52,
+				(d, v) => d.FirstWeekOfYear().AddDays(7*(v-1)),
+				(d,n) => {
+					var firstWeekOfNextYear = d.AddYears(n).FirstWeekOfYear();
+					var zeroBasedWeekOfFirstYear = (d.DayOfYear - d.FirstWeekOfYear().DayOfYear)/7; // should be integral.
+					return firstWeekOfNextYear.AddDays(zeroBasedWeekOfFirstYear*7);
+				}
+			)},
+			{ RecurrSpan.Year  | RecurrSpan.Day, new RUI (365, 
+				(d, v) => d.AddDays (v - d.DayOfYear),
+				(d,n) => d.AddYears(n)
+			)},
+			{ RecurrSpan.Month | RecurrSpan.Week, new RUI (4, 
+				(d, v) => d.AddDays (v * 7 - d.Day),
+				(d,n) => d.AddMonths(n)
+			)},
+			{ RecurrSpan.Month | RecurrSpan.Day, new RUI (28, 
+				(d, v) => d.AddDays (v - d.Day),
+				(d, n) => d.AddMonths(n)
+			)},
+			{ RecurrSpan.Week  | RecurrSpan.Day, new RUI (7, 
+				(d, v) => d.AddDays (v - (d.DayOfWeekStartingMonday()+1) ),
+				(d, n) => d.AddDays(n*7)
+			)},
+		};
+		// check the number of bits present in the flag...
+		public static IEnumerable<RecurrSpan> SplitFlags(this RecurrSpan value)
+		{
+			// BitMasks 101: 0 | 0 == 0, we cant pick up on that "flag" it's the implicit none. 
+			uint current = (uint)value, compare = 1;
+			while (current != 0) {
+				if ((current & compare) != 0) {
+					yield return (RecurrSpan)compare;
+					current -= compare; 
+				}
+				compare *= 2; // next flag...
+			}
+		}
+		public static DateTime FirstWeekOfYear(this DateTime value)
+		{
+			var nval = value.AddDays (-value.DayOfYear);
+			int dow = nval.DayOfWeekStartingMonday();
+			if (dow > 0) // move forward to start of first week, this is the last week in previous year or part of it...
+				nval = nval.AddDays(7-dow);
+			return nval;
+		}
+		public static int DayOfWeekStartingMonday(this DateTime d)
+		{
+			return (d.DayOfWeek == 0 ? 6 : (int)d.DayOfWeek - 1);
+		}
+	}
+	public interface IRecurr
+	{
+		IEnumerable<DateTime> GetOccurances (DateTime Start, DateTime End);
+	}
+	public class RecurrsEveryPattern : IRecurr
+	{
+		// Thse defaults are nice.
+		readonly DateTime FixedPoint; 
+		readonly DateTime? lowerBound, upperBound;
+		readonly int frequency;
+		readonly RecurrSpan units;
+
+		public RecurrsEveryPattern(DateTime fixedPoint, int frequency, RecurrSpan units, DateTime? lowerBound, DateTime? upperBound)
+		{
+			this.FixedPoint = fixedPoint;
+			this.frequency = frequency;
+			this.units = units;
+			this.lowerBound = lowerBound;
+			this.upperBound = upperBound;
+		}
+		#region IRecurr implementation
+		public IEnumerable<DateTime> GetOccurances (DateTime Start, DateTime End)
+		{
+			DateTime useStart = lowerBound.HasValue ? Start > lowerBound.Value ? Start : lowerBound.Value : Start;
+			DateTime useEnd = upperBound.HasValue ? End < upperBound.Value ? End : upperBound.Value : End;
+			DateTime forming = Helpers.Moduliser [units].ClosestToAt (FixedPoint, useStart, frequency);
+			if (forming < Start)
+				forming = Helpers.Moduliser [units].NextValue (forming, FixedPoint, frequency);
+			while (forming <= useEnd) {
+				yield return forming;
+				forming = Helpers.Moduliser [units].NextValue (forming, FixedPoint, frequency);
+			}
+		}
+		#endregion
+	}
+	public class RecurrsOnPattern : IRecurr
+	{
+		readonly DateTime PatternStarts; 
+		readonly DateTime PatternEnds;
+		readonly IList<RecurrSpan> units;
+		readonly int[] onIndexes;
+
+		// for each flag in the unitsMask, we take and index and say of.  
+		// on the  idx[0] flag[0] of the idx[1] flag[1] of the idx[2] flag[2] of the flag[3]
+		// eg on the 1st day of the 2nd week of the 7th month of the year.
+		// so there must always be one more flag set than length of indexes.
+		public RecurrsOnPattern (int[] onIndexes, RecurrSpan unitsMask, DateTime patternStart, DateTime patternEnd)
+		{
+			if(onIndexes.Length == 0) throw new ArgumentException("There must be some indexes specified", "onIndexes.Length");
+
+			// Get split list of flags (backing down not dealing directly with the mask :-(, but cant think how right now)
+			List<RecurrSpan> unitsLocal = new List<RecurrSpan> (unitsMask.SplitFlags ());
+
+			// there must be an "onindex" for each present in the onUnitsMask, not more not less.
+			if(unitsLocal.Count != onIndexes.Length) throw new ArgumentException("Number of indexes be equal to number of flags set in the mask. (1,3) (day|year) means first day of year every 3 years or (4,5)(week|month) start of every 4th week of every fifth month. ", "unitsMask");
+			// we gotta make sure that each "on" staisfies the allowable maximum for the span type.
+			for (int i = 0; i < unitsLocal.Count - 1; i++)
+				if (onIndexes [i] > Helpers.Units [unitsLocal [i] | unitsLocal [i + 1]].MaxValue || onIndexes[i] <=0)
+					throw new ArgumentException ("cant repeat on the " + onIndexes [i] + " " + unitsLocal[i] + " of every " + unitsLocal [i + 1], "onIndexes[" + i + "]");
+
+			// Ok, we can give you an object.
+			this.units = unitsLocal;
+			this.onIndexes = onIndexes;
+			PatternStarts = patternStart;
+			PatternEnds = patternEnd;
+		}
+
+		#region IRecurr implementation
+		public IEnumerable<DateTime> GetOccurances (DateTime Start, DateTime end )
+		{
+			// Firstly, we might be before the pattern actually starts, limit that.
+			if(Start < PatternStarts) Start = PatternStarts;
+
+			// go back through the indexes - we spam datetimes here, there should be a better way.
+			// we are computing like: "Set month of year to x, then set week of month to y, then set day of week to z"
+			// or if you like with one less flag eg. "Set month of year to x, then set day of month to y" 
+			// and any other combinaton you can think of.
+			DateTime forming = Start;
+			DateTime startTrack = Start;
+			for (int i = onIndexes.Length-2; i >=0;i--) // last one is the frequency multiplier
+				forming = Helpers.Units [units [i+1] | units [i]].CreateAtValue (forming, onIndexes [i]);
+
+			int frequency = onIndexes [onIndexes.Length - 1];
+
+			Action Incrementor = () => {
+				bool inc = true;
+				for (int i = onIndexes.Length-2; i >=0;i--) // last one is the frequency multiplier
+				{
+					var use = Helpers.Units[units [i+1] | units [i]];
+					if(inc) forming = startTrack = use.NextValue(startTrack,frequency);
+					forming = use.CreateAtValue(forming, onIndexes[i]);
+					inc = false; // now use createat to position correctly.
+				}
+			};
+
+			// this gets a candidate start value, that might actually be before start, in which case we need to increment by one.
+			if(forming < Start) Incrementor();
+
+			// then we go untill we pass this ending value
+			var formEnd = end;
+			if (formEnd > PatternEnds) formEnd = PatternEnds;
+
+			// by consecutive increment and yielding
+			while(forming <= formEnd)
+			{
+				yield return forming;
+				Incrementor ();
+			} 
+		}
+		#endregion
+	}
+}
