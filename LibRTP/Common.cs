7fb74476a2b71a0708fc76cc12d02f594788767e
David Higgins <d_higgins@imap.cc>
2017-05-21 17:09:54 +0100

    nices

--- LibRTP/Common.cs
+++ LibRTP/Common.cs
@@ -25,26 +25,22 @@ namespace LibRTP
 				default: throw new ArgumentException ();
 			}
 		}
-	}
-
-	static class OurHelpers
-	{
-		public static DateTime StartOfDay(this DateTime somit)
-		{
-			return new DateTime (somit.Year, somit.Month, somit.Day);
-		}
-		public static DateTime FirstWeekOfYear(this DateTime value)
-		{
-			var nval = value.AddDays (-value.DayOfYear);
-			int dow = nval.DayOfWeekStartingMonday();
-			if (dow > 0) // move forward to start of first week, this is the last week in previous year or part of it...
-				nval = nval.AddDays(7-dow);
-			return nval;
-		}
-		public static int DayOfWeekStartingMonday(this DateTime d)
-		{
-			return (d.DayOfWeek == 0 ? 6 : (int)d.DayOfWeek - 1);
-		}
-	}
+        public static DateTime StartOfDay(this DateTime somit)
+        {
+            return new DateTime(somit.Year, somit.Month, somit.Day);
+        }
+        public static DateTime FirstWeekOfYear(this DateTime value)
+        {
+            var nval = value.AddDays(-value.DayOfYear);
+            int dow = nval.DayOfWeekStartingMonday();
+            if (dow > 0) // move forward to start of first week, this is the last week in previous year or part of it...
+                nval = nval.AddDays(7 - dow);
+            return nval;
+        }
+        public static int DayOfWeekStartingMonday(this DateTime d)
+        {
+            return (d.DayOfWeek == 0 ? 6 : (int)d.DayOfWeek - 1);
+        }
+    }
 }
 
